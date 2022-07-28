28b1e5e10ef3bc144fe69d04b4c47a58d7dcaeae
abhishekgarfield <abhishek2759@gmail.com>
2022-07-28 13:22:19 +0530

    post

--- firstapp.js
+++ firstapp.js
@@ -1,8 +1,25 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
 
 app.get('/home', function(req, res){
    res.send("Hello world!");
 });
+
+app.listen(3000);
+*/
+
+var express=require("express");
+var app=express();
+
+app.get("/home",function(req,res)
+{
+   res.send("heloo");
+});
+
+app.post("/home",function(req,res)
+{
+   res.send('you called a post method \n' );
+});
+
 
 app.listen(3000);
