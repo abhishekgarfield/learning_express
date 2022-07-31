2afa00ff36b82e20f6749ea82144b4039eac048a
abhishekgarfield <abhishek2759@gmail.com>
2022-07-31 11:30:16 +0530

    form data

--- firstapp.js
+++ firstapp.js
@@ -124,4 +124,27 @@ app.get("/",function(req,res)
 
 var express=require("express");
 var app=express();
-var upload= require("multer");
+var multer= require("multer");
+var upload=multer();
+var bodyParser=require("body-parser");
+
+app.get("/",(req,res)=>{
+   res.render("form");
+});
+app.set("view engine","pug");
+app.set("views","./views");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(upload.array());
+app.use(express.static("upload"));
+
+app.post("/",(req,res)=>{
+   console.log(req.body);
+   res.send("request recienved");
+});
+app.listen(3000);
+
+
+
