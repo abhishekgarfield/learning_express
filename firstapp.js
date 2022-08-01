845e16f1888bf40e28b273d4518a5eac07a05712
abhishekgarfield <abhishek2759@gmail.com>
2022-08-01 14:09:39 +0530

    comment added

--- firstapp.js
+++ firstapp.js
@@ -81,7 +81,7 @@ app.get("/:name",function(req,res,next)
 app.listen(3000);
 
 */
-
+/*
 var express=require("express");
 var app=express();
 app.set("view engine","pug");
@@ -96,6 +96,7 @@ app.get("/",function(req,res)
    };
    res.render("gh",obj);
 }).listen(3000);
+*/
 
 
 /*
@@ -180,6 +181,9 @@ app.post("/",function(req,res)
 });
 app.listen(3000);
 
+*/
+
+/*
 
 var formidable=require("formidable");
 var http=require("http");
@@ -195,6 +199,59 @@ http.createServer(function(req,res)
    }
 });
 */
+/*
+
+var express=require("express");
+var bodyParser=require("body-parser");
+var multer=require("multer");
+var upload=multer();
+var app=express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/data');
+app.set("view engine","pug");
+app.set("views","./views");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(upload.array());
+app.use(express.static("public"));
+
+var personSchema = mongoose.Schema({
+   name: String,
+   age: Number,
+   nationality: String
+});
+var cars = mongoose.model("cars", personSchema);
+
+app.get('/', function(req, res){
+   res.render('person');
+});
+
+app.post('/person', function(req, res){
+   var personInfo = req.body; //Get the parsed information
+   
+   if(!personInfo.name || !personInfo.age || !personInfo.nationality){
+      res.render('show_message', {
+         message: "Sorry, you provided worng info", type: "error"});
+   } else {
+      var newPerson = new cars({
+         name: personInfo.name,
+         age: personInfo.age,
+         nationality: personInfo.nationality
+      });
+		
+      newPerson.save(function(err, Person){
+         if(err)
+            res.render('show_message', {message: "Database error", type: "error"});
+         else
+            res.render('show_message', {
+               message: "New person added", type: "success", person: personInfo});
+      });
+   }
+});
+app.listen(3000);
+*/
+
+
 
 
 
